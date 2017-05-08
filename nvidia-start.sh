919c22a7cb2892267a30647b9d30954568c8ffe9
Shingo Omura <everpeace@gmail.com>
2017-05-09 01:53:32 +0900

    start all device nodes at nvidia-start.sh

--- nvidia-start.sh
+++ nvidia-start.sh
@@ -4,4 +4,10 @@
 
 # Start the first devices
 /usr/bin/mknod -m 666 /dev/nvidiactl c 195 255 2>/dev/null
-/usr/bin/mknod -m 666 /dev/nvidia0 c 195 0 2>/dev/null
+NVDEVS=`lspci | grep -i NVIDIA`
+N3D=`echo "$NVDEVS" | grep "3D controller" | wc -l`
+NVGA=`echo "$NVDEVS" | grep "VGA compatible controller" | wc -l`
+N=`expr $N3D + $NVGA - 1`
+for i in `seq 0 $N`; do
+  mknod -m 666 /dev/nvidia$i c 195 $i
+done
